6ca936c2e7cfa6cc5d683f794b15902df2032259
hamid <hamid.shafikani@gmail.com>
2020-02-08 00:41:12 +0100

    fddd

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,27 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import {NgModule} from '@angular/core';
+import {Routes, RouterModule} from '@angular/router';
+import {HomeComponent} from './home/home.component';
+import {NotFoundComponent} from './not-found/not-found.component';
+import {SalePointsComponent} from './sale-points/sale-points.component';
 
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  {path: '', component: HomeComponent},
+  {path: 'salepoint', component: SalePointsComponent},
+  {path: '404', component: NotFoundComponent},
+  {path: '**', redirectTo: '/404'}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {
+}
+
+export const routingComponents = [
+  HomeComponent,
+  NotFoundComponent,
+  SalePointsComponent
+];
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,36 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { AppRoutingModule, routingComponents } from './app-routing.module';
+import { FormsModule } from '@angular/forms';
 
-import { AppRoutingModule } from './app-routing.module';
+
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HomeComponent } from './home/home.component';
+import { NotFoundComponent } from './not-found/not-found.component';
+import { SalePointsComponent } from './sale-points/sale-points.component';
+import { SalePointListComponent } from './sale-points/sale-point-list/sale-point-list.component';
+import {SalePointComponent} from './sale-points/sale-point/sale-point.component';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MaterialModule} from './material/material.module';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    routingComponents,
+    HomeComponent,
+    NotFoundComponent,
+    SalePointsComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    FormsModule,
+    MaterialModule,
+    AppRoutingModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
